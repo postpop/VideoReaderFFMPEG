addaaa717f0bc1ef461f91212a458421556e2ac1
Jan Clemens <clemensjan@googlemail.com>
2015-04-18 23:50:09 -0400

    added experimental buffered mode and benchmark

--- VideoReaderFFMPEG.m
+++ VideoReaderFFMPEG.m
@@ -1,7 +1,7 @@
 classdef VideoReaderFFMPEG < handle
-   % For reading videos - needed it since the builting VIDEOREADER wouldn't 
+   % For reading videos - needed it since the builting VIDEOREADER wouldn't
    % accept h264 encoded files - this is a simple command line wrapper for FFMPEG
-   % 
+   %
    % side-note: 2-2.5x faster than builtin VIDEOREADER
    %
    % Exposes a simple interface that implements a subset of the builtin
@@ -24,10 +24,16 @@ classdef VideoReaderFFMPEG < handle
       FrameRate
       Width, Height
       Channels
+      bufferedFrameTimes
+      buffered
+      bufferSize
    end
    
    methods (Access='public')
       function obj = VideoReaderFFMPEG(vFileName)
+         if ~exist(vFileName,'file')
+            error('video file %s does not exist.', vFileName);
+         end
          obj.vFileName = vFileName;
          % add location of FFMPEG/FFPROBE to path
          path1 = getenv('PATH');
@@ -45,16 +51,27 @@ classdef VideoReaderFFMPEG < handle
             obj.(keysField{idx}) = sscanf(out(Index(1) + length(key):end), '%g', 1);
          end
          obj.Channels = size(obj.read(1),3);
+         %
+         obj.bufferedFrameTimes = [];
+         obj.buffered = false;
+         obj.bufferSize = 10;
       end
       
       function frame = read(obj, frameNumber)
          % frame = read(frameNumber);
          % frames = read([startFrameNumner endFrameNumber]);
+         %
+         % direct or buffered (experimental, set obj.buffered=true) reading of frames
          
          frameNumber = frameNumber - 1;% zero-based
          if length(frameNumber)==1
             frameTime = (frameNumber)./obj.FrameRate - 0.1/obj.FrameRate;
-            frame = obj.readSingleFrame(frameTime);
+            if obj.buffered
+               frame = obj.readSingleFrameBuffered(frameTime);
+            else
+               frame = obj.readSingleFrame(frameTime);
+            end
+            
             
             if length(size(frame))==2
                frame = reshape(frame, [size(frame),1,1]);
@@ -63,14 +80,25 @@ classdef VideoReaderFFMPEG < handle
             frameTimes = linspace(frameNumber(1)./obj.FrameRate, 1./obj.FrameRate, frameNumber(2)./obj.FrameRate);
             frame = zeros(obj.Width,obj.Height,obj.Channels,length(frameTimes),'uint8');
             for f = 1:length(frameTimes)
-               frame(:,:,:,f) = obj.readSingleFrame(frameTimes(f));
+               if obj.buffered
+                  frame(:,:,:,f) = obj.readSingleFrameBuffered(frameTimes(f));
+               else
+                  frame(:,:,:,f) = obj.readSingleFrame(frameTimes(f));
+               end
             end
          end
-         
       end
+      
+      function clean(obj)
+         % deletes temporary tifs from disk - make this safe so we don't
+         % accidentally delete files
+         
+         % delete('tmp.tif')
+      end
+      
    end
    
-   methods (Access='private')   
+   methods (Access='private')
       function frame = readSingleFrame(obj, frameTime)
          % write RAW frame to file using FFMPEG - frames are accessed based
          % on time starting with 0 (so frame #1 is 0, not 1/fps!!).
@@ -82,10 +110,27 @@ classdef VideoReaderFFMPEG < handle
          
          evalc(['!ffmpeg -y -ss ' num2str(frameTime, '%1.8f') ' -i ' obj.vFileName ' -v error -vframes 1 tmp.tif']);
          frame = permute(imread('tmp.tif'), [2 1 3]);
-         try
-            delete('tmp.tif')
-         end
+         %          delete('tmp.tif')
+      end
+      
+      function frame = readSingleFrameBuffered(obj, frameTime)
+         % write RAW frame to file using FFMPEG - frames are accessed based
+         % on time starting with 0 (so frame #1 is 0, not 1/fps!!).
          
+         % -vframes 1   - number of frames to extract
+         % -ss seconds  - start point
+         % -v error     - print only error messages
+         % -y           - say 'YES' to any prompt
+         
+         bufferHits = ismember(obj.bufferedFrameTimes, frameTime);
+         if ~any(bufferHits)
+            obj.bufferedFrameTimes = frameTime + (0:obj.bufferSize-1)/obj.FrameRate;
+            evalc(['!ffmpeg -y -ss ' num2str(frameTime, '%1.8f') ' -i ' obj.vFileName ' -v error -vframes ' int2str(obj.bufferSize) ' tmp%5d.tif']);
+            bufferHits = ismember(obj.bufferedFrameTimes, frameTime);
+         end
+         tifFileName = sprintf('tmp%05d.tif', find(bufferHits));
+         frame = permute(imread(tifFileName), [2 1 3]);
+         %          delete(tifFileName)
       end
    end
 end
--- test.m
+++ test.m
@@ -5,22 +5,27 @@
 clear all, clc, clf
 colormap('gray')
 %% 0. init VIDEOREADER objects
-videoFileName = '150410_1457.avi';%'140731_1422.mp4';
+videoFileName = '140731_1422.mp4';
 vr{1} = VideoReader(videoFileName);
 vr{2} = VideoReaderFFMPEG(videoFileName);
-%% .1 make sure metadata are identical
+vr{2}.buffered = true;
+%% 1. make sure metadata are identical
 PropertyNames = {'NumberOfFrames','FrameRate','Width','Height'};
 disp([class(vr{1}) ' ' class(vr{2})])
 for prop = 1:length(PropertyNames)
    disp([vr{1}.(PropertyNames{prop}), vr{2}.(PropertyNames{prop})]);
 end
 %% 2. load frames using the built-in VIDEOREADER
-framesToRead = round(linspace(10, vr{1}.NumberOfFrames-10,10));
+framesToRead = 1000:1010;%round(linspace(10, vr{1}.NumberOfFrames-10,10));
 for fr = 1:length(framesToRead)
    % read and plot frames
    for vid = 1:length(vr)
       subplot(3,length(framesToRead), (vid-1)*length(framesToRead)+fr)
-      frame{vid} = vr{vid}.read(framesToRead(fr));
+      if vid==2
+         frame{vid} = permute(vr{vid}.read(framesToRead(fr)),[2 1 3]);
+      else
+         frame{vid} = vr{vid}.read(framesToRead(fr));
+      end
       imagesc(frame{vid})
       axis('square','off')
    end
@@ -36,4 +41,26 @@ for fr = 1:length(framesToRead)
       ylabel('FFMPEG')
    end
 end
+%%
+tt = zeros(1000,1);
 
+videoFileName = '140731_1422.mp4';
+vr = VideoReaderFFMPEG(videoFileName);
+vr.buffered = false;
+
+for f = 1:1000
+   tic
+   vr.read(5000+f);
+   tt(f,1) = toc;
+end
+%%
+clear vr;
+vr = VideoReaderFFMPEG(videoFileName);
+vr.buffered = true;
+vr.bufferSize = 10;
+for f = 1:1000
+   tic
+   vr.read(4000+f);
+   tt(f,2) = toc;
+end
+plot(1000*tt)
