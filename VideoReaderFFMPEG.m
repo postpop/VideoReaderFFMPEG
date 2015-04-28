e7a3a60242ab34972c0b88d6652e366a268252e0
Jan Clemens <clemensjan@googlemail.com>
2015-04-28 19:26:59 -0400

    fixed tmp file name to avoid buffer collisions

--- VideoReaderFFMPEG.m
+++ VideoReaderFFMPEG.m
@@ -2,20 +2,30 @@ classdef VideoReaderFFMPEG < handle
    % For reading videos - needed it since the builting VIDEOREADER wouldn't
    % accept h264 encoded files - this is a simple command line wrapper for FFMPEG
    %
-   % side-note: 2-2.5x faster than builtin VIDEOREADER
+   % side-note: 2-2.5x faster on an SSD drive than the builtin VIDEOREADER
    %
    % Exposes a simple interface that implements a subset of the builtin
    %  CONSTRUCTOR:
-   %     vr = VideoReaderFFMPEG('test.mp4', ['tempFolder'= './'], ['FFMPEGPath'= '/usr/local/bin' (UNIX) or 'C:\Program Files\ffmpeg\bin' (WIN)]);
+   %     vr = VideoReaderFFMPEG(fileName, ['tempFolder', tempFolder, 'FFMPEGPath', FFMPEGPath]);
+   %     PARAMS:
+   %        fileName    - ...
+   %        tempFolder  - OPTIONAL - location to store temporary files, defaults to './'
+   %        FFMPEGPath  - OPTIONAL - location of FFMPEG/FFPROBE executables defaults to '/usr/local/bin' (UNIX) or 'C:\Program Files\ffmpeg\bin' (WIN)
    %
    %  METHODS:
-   %     read() with single frames or a range of frames [startFrame endFrame]
+   %     read(frames) - with single frames or a range of frames [startFrame endFrame]
+   %     clean()      - NOT IMPLEMENTED - delete all temporary files
    %
    %  PROPERTIES:
    %     Width, Height, NumberOfFrames, FrameRate, Channels
    %
+   %  see also VIDEOREADER
    
    % JC - created 2015/04/03
+   
+   % TODO
+   % - generate random basename instead of 'tmp' to avoid collisions of
+   %   readers simultaneously working in the same folder/on the same file
    
    properties
       % input parameters:
@@ -31,12 +41,15 @@ classdef VideoReaderFFMPEG < handle
       buffered
       bufferSize
       bufferedFrameTimes
+      tempName                % name for temporary tiff files - to avoid buffer collision if multiple instances of VideoReaderFFMPEG access the same tempFolder
       % unused - delete?
       vr
    end
    
    methods (Access='public')
       function obj = VideoReaderFFMPEG(vFileName, varargin)
+         
+         % parse input arguments
          p = inputParser;
          addRequired(p,'vFileName', @ischar);
          defaultTempFolder = '.';
@@ -58,22 +71,23 @@ classdef VideoReaderFFMPEG < handle
          obj.tempFolder = p.Results.tempFolder;
          obj.FFMPEGPath= p.Results.FFMPEGPath;
          
+         obj.tempName = tempname(obj.tempFolder);
+         
          % add location of FFMPEG/FFPROBE to path
-         path1 = getenv('PATH');
+         path = getenv('PATH');
          if isunix
-            path1 = [path1 ':' obj.FFMPEGPath];% this is where FFPROBE is installed to on my system
+            path = [path ':' obj.FFMPEGPath];% this is where FFPROBE is installed to on my system
          end
          if ispc % WINDOWS
-            path1 = [path1 ';' obj.FFMPEGPath ';'];%
+            path = [path ';' obj.FFMPEGPath ';'];%
          end
-         setenv('PATH', path1);
+         setenv('PATH', path);
          % check that FFMPEG and FFPROBE are available in path
-         % system('..') should return 0 but returns 1, and the statement
-         % prints the result
+         % TO FIX: system('..') should return 0 if it exists but returns 1, and the statement prints the result
          %assert(system('ffmpeg')<=1, 'FFMPEG not found!  Use FFMPEGPath parameter to point to binary');
          %assert(system('ffprobe')<=1, 'FFPROBE not found! Use FFMPEGPath parameter to point to binary');
          
-         % get metadata
+         % get metadata - TODO put into its own private/public(?) function
          out = evalc(['!ffprobe -show_streams ' obj.vFileName]);
          out(strfind(out, '=')) = [];
          keys = {'nb_frames', 'width', 'height', 'r_frame_rate'};
@@ -84,6 +98,7 @@ classdef VideoReaderFFMPEG < handle
             obj.(keysField{idx}) = sscanf(out(Index(1) + length(key):end), '%g', 1);
          end
          obj.Channels = size(obj.read(1),3);
+         
          %
          obj.bufferedFrameTimes = [];
          obj.buffered = false;
@@ -141,8 +156,8 @@ classdef VideoReaderFFMPEG < handle
          % -v error     - print only error messages
          % -y           - say 'YES' to any prompt
          
-         evalc(['!ffmpeg -y -ss ' num2str(frameTime, '%1.8f') ' -i ' obj.vFileName ' -v error -vframes 1 ' fullfile(obj.tempFolder, 'tmp.tif')]);
-         frame = imread(fullfile(obj.tempFolder, 'tmp.tif'));
+         evalc(['!ffmpeg -y -ss ' num2str(frameTime, '%1.8f') ' -i ' obj.vFileName ' -v error -vframes 1 ' obj.tempName '.tif']);
+         frame = imread([obj.tempName, '.tif']);
          %          delete('tmp.tif')
       end
       
@@ -158,10 +173,10 @@ classdef VideoReaderFFMPEG < handle
          bufferHits = ismember(obj.bufferedFrameTimes, frameTime);
          if ~any(bufferHits)
             obj.bufferedFrameTimes = frameTime + (0:obj.bufferSize-1)/obj.FrameRate;
-            evalc(['!ffmpeg -y -ss ' num2str(frameTime, '%1.8f') ' -i ' obj.vFileName ' -v error -vframes ' int2str(obj.bufferSize) ' ' fullfile(obj.tempFolder, 'tmp%5d.tif')]);
+            evalc(['!ffmpeg -y -ss ' num2str(frameTime, '%1.8f') ' -i ' obj.vFileName ' -v error -vframes ' int2str(obj.bufferSize) ' ' obj.tempName '%5d.tif']);
             bufferHits = ismember(obj.bufferedFrameTimes, frameTime);
          end
-         tifFileName = sprintf(fullfile(obj.tempFolder, 'tmp%05d.tif'), find(bufferHits,1,'first'));
+         tifFileName = sprintf([obj.tempName, '%05d.tif'], find(bufferHits,1,'first'));
          frame = imread(tifFileName);
          %          delete(tifFileName)
       end
